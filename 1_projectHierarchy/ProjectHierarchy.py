95128e52ac6ad80ea256de0df7288fed8d154b9a
Bryce Larsen <brycelarsen@bryces-mbp.lan>
2021-09-15 21:45:04 -0700

    :truck: Move script

--- 1_projectHierarchy/ProjectHierarchy.py
+++ 1_projectHierarchy/ProjectHierarchy.py
@@ -9,12 +9,16 @@ def getCredentials(env):
     return os.environ.get('tokenName'), os.environ.get('tokenSecret'), os.environ.get('url'), os.environ.get('site')
 
 def getProjects():
+    # Get all projects
     all_projects = list(TSC.Pager(server.projects))
     project_list = []
     for project in all_projects:
         project_list.append((project.name, project.id, project.parent_id))
+    # Create dataframe of relevant attributes
     project_df = pd.DataFrame(data=project_list, columns=['ProjectNM', 'ProjectID', 'ParentProjectID'])
+    # Map project to parent project
     project_to_parent = {project.id: project.parent_id for project in all_projects}
+    # Map project to name
     project_to_name = {project.id: project.name for project in all_projects}
     return project_df, project_to_parent, project_to_name
 
@@ -22,7 +26,9 @@ def getHierarchy(project_id, parent_list=None):
     if parent_list == None:
         parent_list = []
     parent_list.append(project_id)
+    # reset project id to parent project id
     project_id = project_to_parent[project_id]
+    # if there's a parent id, loop through again
     if project_id:
         getHierarchy(project_id, parent_list)
     return parent_list
